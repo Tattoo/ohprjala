dff51d5645fe1c122286c13fa454d0ae7c925923
esko.piirainen@kolumbus.fi <esko.piirainen@kolumbus.fi@24e0c353-e662-12d6-fb6e-d494f668fc76>
2010-04-07 22:15:32 +0000

    nop refact

--- Laskin/fi/hy/laskin/main/Main.java
+++ Laskin/fi/hy/laskin/main/Main.java
@@ -9,13 +9,17 @@ public class Main {
     public static void main(String args[]) {
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
+            	
             	View view = new View_Implementation();
             	Controller controller = new Controller_Implementation();
             	Calculator calculator = new Calculator_Imple();
+            	
             	view.assignController(controller);
             	controller.assignView(view);
             	controller.assignModel(calculator);
+            	
             	view.setVisible();
+            	
             }
         });
     }
